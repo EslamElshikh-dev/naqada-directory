55b278372ab15c9301fb5bda98d4a80ba4052ce3
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-03 10:08:23 +0300

    Fix direct routes for static hosting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   poweredByHeader: false,
   env: {
     NEXT_PUBLIC_SITE_URL: 'https://naqada-directory.vercel.app',
