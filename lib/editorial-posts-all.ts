88a70a6247d98d4c5095d27627d5b894095b317e
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-05 07:39:53 +0300

    Apply inline image enrichment to editorial posts

--- lib/editorial-posts-all.ts
+++ lib/editorial-posts-all.ts
@@ -2,13 +2,16 @@ import { editorialPosts as firstEditorialPosts } from './editorial-posts';
 import { editorialPostsBatch2A } from './editorial-posts-batch-2a';
 import { editorialPostsBatch2B } from './editorial-posts-batch-2b';
 import { editorialPostsBatch2C } from './editorial-posts-batch-2c';
+import { enrichEditorialPostImages } from './editorial-post-image-enrichment';
 
-export const allEditorialPosts = [
+const baseEditorialPosts = [
   ...firstEditorialPosts,
   ...editorialPostsBatch2A,
   ...editorialPostsBatch2B,
   ...editorialPostsBatch2C,
 ];
+
+export const allEditorialPosts = baseEditorialPosts.map(enrichEditorialPostImages);
 
 export function getAllEditorialPost(slug: string) {
   return allEditorialPosts.find((post) => post.slug === slug) || null;
