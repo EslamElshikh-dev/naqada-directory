5ca8c6e66a346f7494be3bb7f8f5cf9129306f03
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-05 07:02:17 +0300

    Register illustrated editorial media assets

--- app/blog-media/[asset]/route.ts
+++ app/blog-media/[asset]/route.ts
@@ -1,6 +1,15 @@
 const assets: Record<string, string> = {
   'bashlaw-today-hero.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/d8cb3e7e-a2a5-4006-a63a-bac16e1ca39f.jpg',
   'bashlaw-quran-learning.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/e6963a37-bed4-46ad-8ae2-cd2e35b81537.jpg',
+  'asmant-mawlid-marmah.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/5c05d488-1cc1-4fb3-ae2c-073d6977c49f.jpg',
+  'sahil-bashlaw-hawawra.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/fea96d12-0ff8-4334-9dbf-34f283748d29.jpg',
+  'daraw-community.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/d39aca80-ec6f-471b-9618-f933f8c712c0.jpg',
+  'sahil-daraw-sheikh-fakhry.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/ccc763ad-21f1-4630-9fe6-9c06173affd6.jpg',
+  'naj-al-qarya-quran-community.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/7918c38f-e35c-4d57-aa91-1397d161e3e2.jpg',
+  'naj-al-sadr-community.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/a66c8b72-e2e5-473e-92f6-cc43c43a70e7.jpg',
+  'awsat-qamula-network.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/4443d10f-e088-4760-b628-b1eb6fa38cec.jpg',
+  'hager-tukh-pottery.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/43e522bf-1d65-4961-8f32-27c24e190ec2.jpg',
+  'naqada-pottery-heritage.jpg': 'https://cdn.creativeclaw.co/u/34cb5082/images/4fcfe0e3-569e-44a2-99ce-ee8724bab063.jpg',
 };
 
 export const revalidate = 604800;
