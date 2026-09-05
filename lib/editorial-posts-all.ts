5121764513ea0b59a73b90514d3c2991f2911f51
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-05 09:22:59 +0300

    ربط مقال نجع الشيخ علي بالمدونة

--- lib/editorial-posts-all.ts
+++ lib/editorial-posts-all.ts
@@ -6,6 +6,7 @@ import { editorialPostsBatch3A } from './editorial-posts-batch-3a';
 import { editorialPostsBatch3B } from './editorial-posts-batch-3b';
 import { editorialPostsBatch3C } from './editorial-posts-batch-3c';
 import { editorialPostsBatch3D } from './editorial-posts-batch-3d';
+import { editorialPostsBatch3E } from './editorial-posts-batch-3e';
 import { enrichEditorialPostImages } from './editorial-post-image-enrichment';
 
 const baseEditorialPosts = [
@@ -17,6 +18,7 @@ const baseEditorialPosts = [
   ...editorialPostsBatch3B,
   ...editorialPostsBatch3C,
   ...editorialPostsBatch3D,
+  ...editorialPostsBatch3E,
 ];
 
 export const allEditorialPosts = baseEditorialPosts.map(enrichEditorialPostImages);
