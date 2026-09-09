c4b273347bfe8b4c7b9dc38df2f99ec00866d2b7
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-09 07:32:38 +0300

    feat(ui): enable discovery experience v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import './mobile-header-nav-fix.css';
 import './interface-refresh.css';
 import './design-system-v2.css';
 import './detail-experience-v2.css';
+import './discovery-experience-v2.css';
 
 const notoKufi = localFont({
   src: './fonts/noto-kufi-arabic.woff2',
