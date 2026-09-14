2efbf8e78d43e93ac7dc7f85edd0e05d33658ff4
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-14 03:19:36 +0300

    style: enable Design V18 foundation

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import './detail-experience-v2.css';
 import './discovery-experience-v2.css';
 import './ui-quality-v13.css';
 import './stability-overrides.css';
+import './design-v18.css';
 
 const notoKufi = localFont({
   src: './fonts/noto-kufi-arabic.woff2',
