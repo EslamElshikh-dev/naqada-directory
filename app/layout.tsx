efaac1a9728950440d411aee457a4b3424b4baf1
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-14 03:27:48 +0300

    style: load V18 responsive hardening

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import './discovery-experience-v2.css';
 import './ui-quality-v13.css';
 import './stability-overrides.css';
 import './design-v18.css';
+import './design-v18-responsive.css';
 
 const notoKufi = localFont({
   src: './fonts/noto-kufi-arabic.woff2',
